79806c7769af0e871700ec793fc041c4a0465343
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-17 16:43:04 +0500

    update: sign-up controller set

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,45 @@ const { OAuth2Client } = require("google-auth-library");
 const oauth2Client = new OAuth2Client();
 
 //sign-up
-exports.signup = async (req, res) => {};
+exports.signup = async (req, res) => {
+  try {
+    const userObj = {
+      name: req.body.name,
+      email: req.body.email,
+      encryptedPassword: req.body.password,
+    };
+
+    // Check if the user already exists
+    const userResponse = await User.findOne({ email: userObj.email });
+
+    console.log("A");
+
+    if (userResponse) {
+      return res.status(409).json("User already exists");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(userObj.encryptedPassword, 10);
+
+    userObj.encryptedPassword = hashedPassword;
+
+    // Create the user with the hashed password
+    console.log("B");
+    const newUser = await User.create(userObj);
+    console.log("C");
+
+    // Remove password from the response
+    const { encryptedPassword, ...userWithoutPassword } = newUser.toObject();
+
+    res.status(200).json({
+      message: "User Created Successfully",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal Server Error");
+  }
+};
 
 //login
 exports.login = async (req, res) => {};
