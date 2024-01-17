2234aa07a51db9f56bcabf77840d90fe6faac6df
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-17 16:45:31 +0500

    update: googleAuth controller set

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,4 +99,83 @@ exports.logout = async (req, res) => {
 exports.forgetPassword = async (req, res) => {};
 
 //Google Oauth
-exports.googleAuth = async (req, res) => {};
+exports.googleAuth = async (req, res) => {
+  try {
+    // get the code from frontend
+    const code = req.body.code;
+    console.log("Authorization Code:", code);
+
+    // Exchange the authorization code for an access token
+    const response = await axios.post(
+      "https://accounts.google.com/o/oauth2/token",
+      {
+        code,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: "postmessage",
+        grant_type: "authorization_code",
+      }
+    );
+    const accessToken = response.data.access_token;
+    console.log("Access Token:", accessToken);
+
+    // Fetch user details using the access token
+    const googleUser = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    console.log(googleUser);
+
+    const userResponse = await User.findOne({ email: googleUser.data.email });
+
+    if (!userResponse) {
+      const userObj = {
+        name: googleUser.name,
+        email: googleUser.email,
+        encryptedPassword: "12345678",
+      };
+
+      const hashedPassword = await bcrypt.hash("12345678", 10);
+
+      userObj.encryptedPassword = hashedPassword;
+
+      // Create the user with the hashed password
+      console.log("B");
+      const newUser = await User.create(userObj);
+      console.log("C");
+
+      // Remove password from the response
+      const { encryptedPassword, ...userWithoutPassword } = newUser.toObject();
+
+      const token = jwt.sign(
+        { name: newUser.name, userId: newUser._id },
+        process.env.JWT_SECRET_KEY
+      );
+
+      res.status(200).json({
+        message: "SignUp successful",
+        user: userWithoutPassword,
+      });
+    } else {
+      const { encryptedPassword, ...userWithoutPassword } =
+        userResponse.toObject();
+      const token = jwt.sign(
+        { name: userResponse.name, userId: userResponse._id },
+        process.env.JWT_SECRET_KEY
+      );
+
+      res.status(200).json({
+        message: "Login successful",
+        user: userWithoutPassword,
+      });
+    }
+  } catch (error) {
+    console.error("Error authenticating Google:", error);
+    res.status(500).json({ message: "Error authenticating Google" });
+  }
+};
