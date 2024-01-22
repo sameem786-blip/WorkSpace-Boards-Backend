1a23b636fcfa7279def81fe160b38ef4c509cdec
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-22 16:18:33 +0500

    update: user routes aded to app

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const env = require("dotenv").config();
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
 
 const { connect } = require("./db/config");
 
 const AuthRoutes = require("./routes/auth");
+const UserRoutes = require("./routes/user");
 
 const app = express();
 
@@ -15,21 +15,13 @@ app.use(
     origin: "*",
   })
 );
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
 
-//   next();
-// });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 connect();
 
 app.use("/auth/user/", AuthRoutes);
+app.use("/api/user/", UserRoutes);
 
 module.exports = app;
