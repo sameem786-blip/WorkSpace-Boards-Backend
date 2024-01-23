251e08aaf8a54dd0b3fc425fe3f521a9a17c3b83
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-23 14:33:20 +0500

    update: nodemailer used to send reset pass otp

--- helpers/index.js
+++ helpers/index.js
@@ -7,3 +7,36 @@ exports.testPasswordSyntax = (password) => {
   const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
   return passwordRegex.test(password);
 };
+
+exports.generateOTP = () => {
+  const otp = Math.floor(1000 + Math.random() * 9000);
+  return otp.toString();
+};
+
+exports.generateEmail = async (subject, body, cc, to) => {
+  try {
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = await nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: `"WorkSpace-Boards 👻" <${process.env.SMTP_EMAIL}>`, // sender address
+      to: to, // list of receivers
+      subject: subject, // Subject line
+      html: body, // html body
+      cc: cc,
+    });
+
+    return "Email Sent Sucessfully";
+  } catch (err) {
+    console.log(err);
+  }
+};
