958486614deeda779c6465d244b9b909809759a8
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-08 02:19:56 +0500

    kjnkjn

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
 const userController = require("../controllers/user.js");
+const checkLogin = require("../middlewares/checkLogin.js");
 
-router.put("/updateUsername", userController.updateUsername);
+router.put("/updateUsername",checkLogin, userController.updateUsername);
 router.put("/updateProfilePic", userController.updateProfilePic);
 router.put("/updateFirstname", userController.updateProfilePic);
 router.put("/updateLastname", userController.updateProfilePic);
