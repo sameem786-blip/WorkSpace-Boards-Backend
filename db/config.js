03fbd7797ce7ccaa9fe95b89c435f86b5b9fefe4
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-10 01:07:20 +0500

    frerf

--- db/config.js
+++ db/config.js
@@ -6,7 +6,7 @@ const uri = process.env.MONGO_URI;
 exports.connect = async () => {
   try {
     await mongoose.connect(uri, { connectTimeoutMS: 8000 });
-    console.log("Connected to DB");
+    console.log("Connected to DB")
   } catch (err) {
     console.log(err);
   }
