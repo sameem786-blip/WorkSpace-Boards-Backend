22812155ad6460897695a5c31c20d91e4b35b887
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-10 01:07:38 +0500

    frerf

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const authController = require("../controllers/auth.js");
 
-router.post("/signup", authController.signup);
+router.post("/signup", authController.signup)
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 router.post("/forgetPassword/sendOTP", authController.sendOTP);
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,9 @@ const router = require("express").Router();
 const userController = require("../controllers/user.js");
 const checkLogin = require("../middlewares/checkLogin.js");
 
-router.put("/updateUsername",checkLogin, userController.updateUsername);
-router.put("/updateProfilePic", userController.updateProfilePic);
-router.put("/updateFirstname", checkLogin,userController.updateFirstname);
-router.put("/updateLastname", checkLogin,userController.updateLastname);
+router.put("/updateUsername",checkLogin, userController.updateUsername)
+router.put("/updateProfilePic", userController.updateProfilePic)
+router.put("/updateFirstname", checkLogin,userController.updateFirstname)
+router.put("/updateLastname", checkLogin,userController.updateLastname)
 
 module.exports = router;
