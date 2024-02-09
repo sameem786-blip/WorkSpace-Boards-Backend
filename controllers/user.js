017073d135091ef187c7afd0a0b699fd9c3ed72e
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-09 22:27:49 +0500

    jn

--- controllers/user.js
+++ controllers/user.js
@@ -103,6 +103,54 @@ exports.updateFirstname = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+exports.updateLastname = async (req, res) => {
+  try {
+    console.log("Updating username")
+    const lastName = req.body.lastName;
+    const userId = req.userData.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (lastName && lastName.trim() !== "") {
+      if (lastName.trim() !== user.lastName) {
+        user.lastName = lastName.trim();
+        await user.save();
+
+        const { firstName, lastName, _id, email, profilePic } = user;
+        const token = jwt.sign(
+          {
+            lastName,
+            lastName,
+            lastName: user.lastName,
+            userId: _id,
+            email,
+            profilePic,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "5d" }
+        );
+
+        return res.status(200).json({
+          message: "lastName updated successfully",
+          token,
+        });
+      } else {
+        return res.status(404).json({ message: "No changes made" });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Invalid or empty input provided" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 exports.updateProfilePic = async (req, res) => {
   try {
