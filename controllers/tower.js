00edc0ec5e7fc4007058d50ae77515fe718547b3
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-28 13:06:27 +0500

    ljlk

---
+++ controllers/tower.js
@@ -0,0 +1,10 @@
+const Tower = require("../schemas/Tower");
+
+exports.createTower = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("Internal Server Error")
+    }
+}
